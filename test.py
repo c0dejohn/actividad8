1c3457d802400fe73999e437d863305ceffd4ea0
john.manyoma <john.manyoma@rappi.com>
2020-12-24 02:39:03 -0500

    src22

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest2
+from ejercicio2 import *
+
+class Pruebas(unittest2.TestCase):
+    def test_validar_edad(self):
+        self.assertIsNot(edad,self)
+        
+    
+
+#        self.assertIsInstance(peso,sexo)
+    
+if __name__== "__main__":
+    unittest2.main()
