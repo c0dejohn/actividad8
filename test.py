719bbf9f9b60bf170f56dfd5cb915cdb8a1daff8
c0dejohn <jemanyoma07@gmail.com>
2020-12-24 23:22:57 -0500

    🚀 RELEASE: ejercucio3 added

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import unittest2
-from ejercicio2 import *
+
+from src.ejercicio2 import *
+
 
 class Pruebas(unittest2.TestCase):
     def test_validar_edad(self):
-        self.assertIsNot(edad,self)
-        
-    
+        self.assertIsNot(edad, self)
+
 
 #        self.assertIsInstance(peso,sexo)
-    
-if __name__== "__main__":
+
+if __name__ == "__main__":
     unittest2.main()
